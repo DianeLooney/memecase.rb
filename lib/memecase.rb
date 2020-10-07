0d5a8c4ae5deba002bbac1bc5592d158084694fb
Diane Looney <diane@dianelooney.com>
2020-10-07 15:20:59 -0400

    Update readme.md

--- lib/memecase.rb
+++ lib/memecase.rb
@@ -1,6 +1,11 @@
-require "memecase/version"
-
-module Memecase
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+  def memecase
+    out = clone
+    up = true
+    for i in 0..length-1 do
+      up = !up if rand > 0.3
+      out[i] = up ? self[i].upcase : self[i].downcase
+    end
+    out
+  end
 end
--- lib/memecase/version.rb
+++
@@ -1,3 +0,0 @@
-module Memecase
-  VERSION = "0.1.0"
-end
